0b406b7c294db6b299ebc3ff5e0c85ad69c5cddc
Yskandar Gas <yskandar.gas@gmail.com>
2026-03-28 17:33:57 +0100

    Adding security (login, password hashing, JWT token) support for the API Updating tools.py

--- src/custom_api/api/text_api.py
+++ src/custom_api/api/text_api.py
@@ -1,39 +1,137 @@
+import os
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, HTTPException
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
 
-from src.custom_api.api.users import User, user_registry
+from src.custom_api.api.users import User, load_user_db, update_user_db
 from src.custom_api.core.text_processor import Task, TextProcessor
-from src.custom_api.utils.tools import get_default_llm_config
+from src.custom_api.utils.tools import get_default_llm_config, initialize_environment
+
+# Setup environment and retrieve important environment variables
+initialize_environment()
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"))
+USER_DB = load_user_db()
+
+assert SECRET_KEY, "SECRET_KEY variable missing from constants.env"
+assert ALGORITHM, "ALGORITHM variable missing from constants.env"
+assert (
+    ACCESS_TOKEN_EXPIRE_MINUTES
+), "ACCESS_TOKEN_EXPIRE_MINUTES variable missing from constants.env"
+
 
 app = FastAPI()
 llm_config = get_default_llm_config()
 processor = TextProcessor(llm_config)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+password_hash = PasswordHash.recommended()
 
 
 def identify_user(token: Annotated[str, Depends(oauth2_scheme)]):
     """
     Identify the user using the user registry
     """
-    if user_registry.get(token):
-        user = User.model_validate_json(user_registry.get(token))
-    else:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-    if user.disabled:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        decoded_json = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = decoded_json.get("sub")
+        exp_date = datetime.strptime(
+            decoded_json.get("exp_date"), "%Y-%m-%d %H:%M:%S"
+        ).replace(tzinfo=timezone.utc)
+
+        if username is None:
+            raise credentials_exception
+
+        if datetime.now(timezone.utc) > exp_date:
+            raise HTTPException(
+                status_code=400, detail="Expired token, please login again"
+            )
+
+    except InvalidTokenError:
+        raise credentials_exception
+
+    user = get_user(username=username)
+
+    if user is None:
+        raise HTTPException(status_code=400, detail="User does not exist")
+    elif user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     else:
         return user
 
 
-def hash_user_info(username, userpassword):
+def create_access_token(data: dict, expire_delay: timedelta | None = None):
     """
-    Hash the username+password
+    This function creates a JSON Web Token from the input JSON data
     """
 
-    return username + userpassword
+    copied_data = data.copy()
+    if expire_delay:
+        expiration_date = datetime.now(timezone.utc) + expire_delay
+    else:
+        expiration_date = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    # Integrate the expiration date in the json data
+    copied_data.update({"exp_date": expiration_date.strftime("%Y-%m-%d %H:%M:%S")})
+
+    # Encode
+    encoded_jwt = jwt.encode(copied_data, key=SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+
+def hash_user_password(userpassword: str):
+    """
+    Hash the password
+    """
+    return password_hash.hash(userpassword)
+
+
+def get_user(username: str):
+    return User.model_validate_json(USER_DB.get(username))
+
+
+def create_user(username: str, userpassword: str):
+    """
+    Create a User object and register it in the user_db
+    """
+    global USER_DB
+
+    if USER_DB.get(username) is not None:
+        raise HTTPException(status_code=400, detail="User already exists")
+    else:
+        user = User(username=username, hashed_password=hash_user_password(userpassword))
+        update_user_db(user_db=USER_DB, user=user)
+        USER_DB = load_user_db()
+
+
+def authenticate_user(username: str, userpassword: str):
+    """
+    Authenticates the user in the user_registry using username and password
+    """
+
+    if not USER_DB.get(username):
+        print("User does not exist, creating user with provided username & password")
+        create_user(username, userpassword)
+
+    user = get_user(username=username)
+
+    if password_hash.verify(userpassword, user.hashed_password):
+        return user
+    else:
+        raise HTTPException(status_code=401, detail="Wrond username or password")
 
 
 @app.get("/")
@@ -45,13 +143,20 @@ def root():
 
 @app.post("/token")
 def login(login_form: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    hash = hash_user_info(
+
+    user = authenticate_user(
         username=login_form.username, userpassword=login_form.password
     )
-    if not user_registry.get(hash):
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-    return {"access_token": hash, "token_type": "bearer"}
+    if user.disabled:
+        raise HTTPException(status_code=400, detail="Disabled User")
+
+    # Create access token once user is authenticated
+    exp_delay = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expire_delay=exp_delay
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.get("/users/me")
@@ -77,10 +182,10 @@ def perform_task(
 
 
 @app.get("/text_processing/summarize/{text}")
-def summarize_text(text: str):
+def summarize_text(text: str, current_user: Annotated[User, Depends(identify_user)]):
     return processor.text_summary(text=text)
 
 
 @app.get("/text_processing/analyze/{text}")
-def analyze_text(text: str):
+def analyze_text(text: str, current_user: Annotated[User, Depends(identify_user)]):
     return processor.sentiment_analyzis(text=text)
--- src/custom_api/utils/tools.py
+++ src/custom_api/utils/tools.py
@@ -1,13 +1,26 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 
-def get_default_llm_config(
-    constants_path: str = "/home/yska/Projets/new_skills/module_1/custom_api/constants.env",
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+current_dir = Path(__file__).parent
+env_file = current_dir.parent.parent.parent / "constants.env"
+
+
+def initialize_environment(
+    constants_path: str = env_file,
 ):
-
     load_dotenv(constants_path)
+
+
+def get_default_llm_config():
     config = {
         "model": os.environ.get("MODEL_NAME"),
         "base_url": os.environ.get("MODEL_ENDPOINT"),
